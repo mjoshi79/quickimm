ce3d579f70dd254da295756cee4b6ea95db576c5
pratere@berea.edu <pratere@berea.edu>
2018-03-03 22:33:22 -0500

    Added sumit's data stuff emergency employment and government

---
+++ sumit/emergency.js
@@ -0,0 +1,11 @@
+emergency_contacts:{
+	police:{
+		phoneNumber : 911
+	},
+	embassy:{
+		phoneNumberDict : {"Nepal": 2026674550, "Australia": 2027973000, "Russia": 2022985700, "Pakistan": 2022436500, "Mexico": 2027281600, "Bulgaria": 2023870174, "England": 2025886500, "Uganda": 2027267100} 
+	},
+	suicideHotline:{
+		phoneNumber : 8002738255 
+	}
+}
---
+++ sumit/employment.js
@@ -0,0 +1,25 @@
+employment:{
+	onCampus:{
+		name "On-Campus Employment",
+		visa: "F1 Student Visa",
+		decription: "*20 hours of work per week\n*Full-time work allowed during holidays and vacations",
+		year: ["Freshman", "Sophomore", "Junior", "Senior"],
+	},
+	opt:{
+		name: "OPT (Optional Practical Training)",
+		visa: "F1 Student Visa",
+		description: "OPT: You may work part-time or full-time",
+		year: []
+	}
+	cpt:{
+		name: "CPT (Curricular Practical Training)",
+		visa: "F1 Student Visa",
+		description: "CPT is any alternative work/study, internship, cooperative education, or other type of required internship or practicum that is offered by sponsoring employers through cooperative agreements with the school."
+		year: ["Sophomore","Junior","Senior"]
+	}
+	gettingGreenCard:{
+		visa: "H1-B Visa",
+		description: "H1-B visa can lead to a Green Card.",
+		year: []
+	}
+}
