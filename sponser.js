9d72f89c628efac0f91d8f24e67534fa89618d78
mjoshi79 <hacking@gmail.com>
2018-03-04 00:40:06 -0500

    This is related to sponser info- Malika

---
+++ sponser.js
@@ -0,0 +1,20 @@
+sponser: {
+  Jorge:{
+    name: "Jorge",
+    countryOrigin: "Nepal",
+    location: "Centreville",
+    Accomodation: "I am willing to provide international students shelter for a month until they settle down. You may contact me via email at johndoe@gmail.com",
+  },
+  Kimberly:{
+    name: "Kimberly",
+    countryOrigin: "China",
+    location: "Fairfax",
+    Accomodation: "I am willing to provide international students food for a month until they settle down. You may contact me via email at kim@gmail.com",
+  },
+  Tom:{
+    name: "Tom",
+    countryOrigin: "USA",
+    location: "Falls Church",
+    Accomodation: "I am willing to provide international students food and shelter until they settle down. You may contact me via email at kim@gmail.com",
+  },
+}
