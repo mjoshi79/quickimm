4239c3f3ca9f4b42be8e81bde7e68aade19f567b
Aury <abwashi@hawk.iit.edu>
2018-03-03 19:00:38 -0600

    got a first page to work!

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -7,12 +7,23 @@ module.exports = (app) => {
   app.use('/', router);
 };
 
+
 router.get('/', (req, res, next) => {
   Article.find((err, articles) => {
     if (err) return next(err);
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
+    res.render('welcome', {
+      title: 'QuickImm',
+      // articles: articles
+    });
+  });
+});
+
+router.get('/year', (req, res, next) => {
+  Article.find((err, articles) => {
+    if (err) return next(err);
+    res.render('year', {
+      title: 'Year in school',
+      // articles: articles
     });
   });
 });
