696f2f8da20c485fabd89656ffcc07d16d2e7a0e
mjoshi79 <hacking@gmail.com>
2018-03-03 22:42:20 -0500

    added malika's contribution

---
+++ malika/education.js
@@ -0,0 +1,20 @@
+education:{
+    educationStatus:{
+      credit: "Must be enrolled full-time \n By definition Full time enrollement means that you are enrolled in atleast 12 credit hours each semester. ",
+      offCampusWork: "You could face a serious consequence if you outside the campus without permission. \n However you may work part-time on campus and are required by the law to complete an Form I-9. \n Please contact International Office for this matter. ",
+      Classes: "1. Talk to your advisor. \n 2. Do not limit yourself! Discover yourself by volunteering for organizations. Some great references to finding such opportunity include: \n http://www.dcinternships.org/ \n https://www.volunteermatch.org/search?l=VA . ",
+    },
+      toefl:{
+			name: "TOEFL (Test Of English as Foreign Language)",
+			description: "The TOEFL is an entrance exam used by several top schools to ascertain international students’ aptitude within the English language",
+			format: "Reading\nListening\nSpeaking\nWriting",
+			prep: "TOEFL books\nTOEFL courses\nPractice Tests",
+		},
+		sat:{
+			name: "SAT (Scholastic Assessment Test)",
+			description: "The SAT is a standerdized test fot college admissions in USA",
+			format: "Reading Test\nWriting and Language Test\nMathematics",
+			prep: "SAT Prep Courses\nPractice Books",
+		},
+
+}
